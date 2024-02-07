cc17246eddb301c4b24b73de2a7d17956ab9e478
raimeiga <yusuke612@hotmail.com>
2024-02-08 01:31:31 +0900

    フェードイン機能を追加

--- js/script.js
+++ js/script.js
@@ -16,9 +16,47 @@ close.addEventListener('click', () => {
 //   ここまでヘッダー
 
 
+// フェードイン（左から出てくるように） 
+// white-background-1というidを持つ要素が、画面内に現れたら、その要素にinviewというクラスを付与する」という意味
+// cssの#white-background-1.inview{ }と対応
+$(function () {
+  $("#white-background-1").on("inview", function () {
+      $(this).addClass("inview");
+  });
+});
 
+// フェードイン（右から出てくるように） 
+// white-background-2というidを持つ要素が、画面内に現れたら、その要素にinviewというクラスを付与する」という意味
+// cssの#white-background-2.inview{ }と対応
+$(function () {
+  $("#white-background-2").on("inview", function () {
+      $(this).addClass("inview");
+  });
+});
 
+// 吹き出し1:拡大しながらフェードイン(中央から四隅へ)
+// voice01というidを持つ要素が、画面内に現れたら、その要素にinviewというクラスを付与する」という意味
+// cssの#voice01.inview{ }と対応
+$(function () {
+  $("#voice01").on("inview", function () {
+      $(this).addClass("inview");
+  });
+});
 
+// 吹き出し3:拡大しながらフェードイン(中央から四隅へ)
+// voice02というidを持つ要素が、画面内に現れたら、その要素にinviewというクラスを付与する」という意味
+// cssの#voice01.inview{ }と対応
+$(function () {
+  $("#voice02").on("inview", function () {
+      $(this).addClass("inview");
+  });
+});
 
-
-
+// 吹き出し3:拡大しながらフェードイン(中央から四隅へ)
+// voice03というidを持つ要素が、画面内に現れたら、その要素にinviewというクラスを付与する」という意味
+// cssの#voice01.inview{ }と対応
+$(function () {
+  $("#voice03").on("inview", function () {
+      $(this).addClass("inview");
+  });
+});
